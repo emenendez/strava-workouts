d2ff9b12df94d80e6a1cae34443b4a038a8974b5
Eric Menendez <ericmenendez@gmail.com>
2015-05-29 19:33:44 -0400

    Get Strava auth code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,29 @@
  * Controller of the stravaWorkoutsApp
  */
 angular.module('stravaWorkoutsApp')
-  .controller('MainCtrl', function ($scope) {
+  .config(function($locationProvider) {
+  	$locationProvider.html5Mode(true);
+  })
+  .controller('MainCtrl', function ($scope, $location) {
 
-  	// TODO: Authenticate with Strava here.
+  	// Authenticate with Strava
+  	// 2. Strava redirects here with access code
+  	if ($location.search().code) {
+  		// 3. This page calls lambda function with access code
+  		// 4. Lambda function knows secret, exchanges code for token with secrent
+  		// 5. Lambda function returns the token as a parameter
+  		alert($location.search().code);
+  		
+
+  	}
+  	else {
+  		// 1. Page redirects to strava auth endpoint
+  		// window.location = 'https://www.strava.com/oauth/authorize?client_id=6518&response_type=code&redirect_uri=http://emenendez.github.io/strava-workouts/';
+  		window.location = 'https://www.strava.com/oauth/authorize?client_id=6518&response_type=code&redirect_uri=http://localhost:9000/';
+  		return;
+  	}
+  	
+  	// 
   	// 
     $scope.swims = [1, 2];
     $scope.bikes = [1, 2, 3];
